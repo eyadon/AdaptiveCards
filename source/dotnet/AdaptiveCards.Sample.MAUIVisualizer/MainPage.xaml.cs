2b72be2765238b8f983fc6b054da8345064e0087
Emily Yadon <emily.yadon@mercmarine.com>
2024-08-07 08:09:07 -0500

    Fix a lot of styling issues in one go by locking to light theme :)

--- source/dotnet/AdaptiveCards.Sample.MAUIVisualizer/MainPage.xaml.cs
+++ source/dotnet/AdaptiveCards.Sample.MAUIVisualizer/MainPage.xaml.cs
@@ -19,6 +19,8 @@ namespace AdaptiveCards.Sample.MAUIVisualizer
         {
             InitializeComponent();
 
+            Application.Current.UserAppTheme = AppTheme.Light;
+
 
             var hostConfig = RendererHostConfigHelper.GetConfig();
             //var timer = new System.Timers.Timer(TimeSpan.FromSeconds(1));
